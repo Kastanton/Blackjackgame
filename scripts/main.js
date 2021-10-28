eb25b89cbe3eb43e1ec7e979b7e0d59d67718bf2
Kastanton <Kristin.astant@gmail.com>
2021-10-28 19:32:55 -0400

    changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 window.addEventListener('DOMContentLoaded', function() {
+  const dealBtn = document.querySelector("#deal-button");
+  dealBtn.addEventListener("click", function(){
+    console.log("Hey there");
+
+    const newCard = document.createElement('img');
+    console.log(newCard);
+  });
   // Execute after page load
 })
